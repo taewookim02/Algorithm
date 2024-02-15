5ef1b315782d46f46ed9ef2a06ca063188e8888d
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-02-15 16:30:47 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -4,11 +4,7 @@ class Solution {
         int[] answer = new int[numbers.length];
         
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > 0) {
-                answer[i] = numbers[i] * 2;
-            } else {
-                answer[i] = numbers[i] * 2;
-            }
+            answer[i] = numbers[i] * 2;
         }
         
         return answer;
