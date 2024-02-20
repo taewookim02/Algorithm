5e12496cb89ecd66894025894a3b358f3e568e88
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-02-20 16:14:16 +0900

    [level 0] Title: 모음 제거, Time: 0.17 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        answer = my_string.replace("a", "");
+        answer = answer.replace("e", "");
+        answer = answer.replace("i", "");
+        answer = answer.replace("o", "");
+        answer = answer.replace("u", "");
+        
+        System.out.println(answer);
+        
+        
+
+        return answer;
+    }
+}
