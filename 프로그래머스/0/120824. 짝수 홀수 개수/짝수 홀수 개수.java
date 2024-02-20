48a1eed84ccaf17377f03d06b5bb421915dbfb49
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-02-20 11:44:15 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        int[] answer = new int[] {evenCount, oddCount};
+        
+        return answer;
+    }
+}
