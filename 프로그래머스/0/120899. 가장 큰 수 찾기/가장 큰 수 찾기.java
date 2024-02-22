97cd80775529a222d47783550916310878e2661e
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-02-22 09:37:45 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -6,14 +6,10 @@ class Solution {
         for (int i = 1; i < array.length; i++) {
             if (max < array[i]) {
                 max = array[i];
-            }
-        }
-        
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == max) {
                 maxIndex = i;
             }
         }
+        
         int[] answer = {max, maxIndex};
 
         // get index by max value
