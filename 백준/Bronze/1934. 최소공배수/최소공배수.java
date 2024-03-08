d419854e458d0c2e239708ce37c28379aa221cde
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-08 17:09:03 +0900

    [Bronze I] Title: 최소공배수, Time: 272 ms, Memory: 18440 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc. nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            System.out.println((a * b) / gcd(a, b));
+        }
+    }
+    
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
