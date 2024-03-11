a2609669adacd601e4186f2190ff347e241f567e
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-11 09:16:11 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = {gcd(n, m), lcm(n, m)};
-        
         return answer;
     }
     
-    static private int gcd (int a, int b) {
+    public int gcd(int a, int b) {
         while (b != 0) {
             int r = a % b;
             a = b;
@@ -14,7 +13,7 @@ class Solution {
         return a;
     }
     
-    static private int lcm (int a, int b) {
+    public int lcm(int a, int b) {
         return a * b / gcd(a, b);
     }
 }
