40886b3de8eb165b67dd10958705176869d5a2fe
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-14 16:07:52 +0900

    [level 0] Title: 수 조작하기 2, Time: 706.46 ms, Memory: 376 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+
+        int startNum = numLog[0];
+        for (int i = 1; i < numLog.length; i++) {
+            switch(numLog[i] - numLog[i -1]) {
+                case 1:
+                    answer += "w";
+                    break;
+                case -1:
+                    answer += "s";
+                    break;
+                case 10:
+                    answer += "d";
+                    break;
+                case -10:
+                    answer += "a";
+                    break;
+            }            
+        }
+        return answer;
+    }
+}
