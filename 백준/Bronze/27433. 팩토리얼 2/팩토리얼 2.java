521c608054c6fffe0de2230b54d1784c0897f466
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-15 14:43:07 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        
+        
+        System.out.println(factorial(a));
+    }
+    
+    private static long factorial(long num) {
+        if (num <= 1) {
+            return 1;  
+        } else {
+            return factorial(num - 1) * num;    
+        }
+  
+    }
+}
