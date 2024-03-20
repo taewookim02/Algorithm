a8cc7b8c589489f99e1e79ff76a3c532a7c15dfd
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-20 11:39:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 52.65 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,23 +3,22 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         int[] answer = {};
-        Stack myStack = new Stack();
+        Stack<Integer> myStack = new Stack();
         myStack.push(arr[0]);
         
         for (int i = 1; i < arr.length; i++) {
-            if ((int)myStack.peek() == arr[i]) {
+            if (myStack.peek() == arr[i]) {
                 continue;
             } else {
                 myStack.push(arr[i]);
             }
         }
-        
-        // stack to int arr
         answer = new int[myStack.size()];
-        
         for (int i = answer.length-1; i >= 0; i--) {
-            answer[i] = (int)myStack.pop();
+            answer[i] = myStack.pop();
         }
+        
+        System.out.println(myStack);
         
         return answer;
     }
