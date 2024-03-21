1629c8709333e9b550bd67b335756e5d9650cef4
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-21 09:21:02 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int num1, int num2) {
-        // int answer = 0;
-        return num1 / num2;
+        return recursiveDivision(num1, num2);
+    }
+    
+    private static int recursiveDivision(int dividend, int divisor) {
+        if (dividend < divisor) {
+            return 0;
+        }
+        
+        return 1 + recursiveDivision(dividend - divisor, divisor);
     }
 }
