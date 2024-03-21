1f88a27de046c783dcd1abdc5cdde03be8488530
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-21 14:30:47 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -1,7 +1,21 @@
 class Solution {
     public int solution(int num1, int num2) {
-        // int answer = -1;
+        // return recursiveRemainder(num1, num2);     
+        return recursiveModulo(num1, num2);     
+    }
+    
+    // private static int recursiveRemainder(int dividend, int divisor) {
+    //     if (dividend - divisor <= 0) {
+    //         return divisor - dividend;
+    //     }
+    //     return recursiveRemainder(dividend - divisor, divisor);
+    // }
+    
+    private static int recursiveModulo(int a, int b) {
+        if (a < b) {
+            return a;
+        }
         
-        return num1 % num2;       
+        return recursiveModulo(a - b, b);
     }
 }
