bcea94f10d16ee7114c9f2da4e0ec8f2cedf248c
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-22 17:23:46 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -0,0 +1,22 @@
+-- 코드를 입력하세요
+# 2022년 1월의
+# 카테고리 별 도서 판매량을 합산
+# 카테고리(CATEGORY), 
+# 총 판매량(TOTAL_SALES) 리스트를 출력
+# 카테고리명을 기준으로 오름차순 정렬
+
+# SELECT DATE_FORMAT(SALES_DATE, '%Y-%m') 
+# FROM BOOK_SALES
+# WHERE DATE_FORMAT(SALES_DATE, '%Y-%m')  = '2022-01';
+
+
+SELECT B.CATEGORY, SUM(BS.SALES)
+FROM BOOK B
+JOIN BOOK_SALES BS 
+ON B.BOOK_ID = BS.BOOK_ID
+WHERE DATE_FORMAT(BS.SALES_DATE, '%Y-%m') = '2022-01'
+GROUP BY B.CATEGORY
+ORDER BY B.CATEGORY
+
+;
+
