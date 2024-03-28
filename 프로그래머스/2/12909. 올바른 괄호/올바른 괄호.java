d7f2bd997a279eac8f182ca14f4a6e820e578ad4
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-03-28 17:45:59 +0900

    [level 2] Title: 올바른 괄호, Time: 19.75 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,23 @@
 import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
         Stack<Character> st = new Stack<>();
-    
+        
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(0) == ')') {
-                return false;
-            }
+            if (s.charAt(0) == ')') return false;
+            
             if (s.charAt(i) == '(') {
-                st.push(s.charAt(i));
-            } else if (st.size() > 0) {
+                st.push('(');
+            }
+            if (st.size() > 0 && s.charAt(i) == ')') {
                 st.pop();
             }
         }
-        if (st.size() > 0) {
-            return false;
-        }
+        if (st.size() > 0) return false;
         
-        System.out.println(st);
+        
         return answer;
     }
 }
