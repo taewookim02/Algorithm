9c9f161ab700555c524c38a2f16e7d69132d3e6a
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-04-05 10:34:51 +0900

    [Silver IV] Title: ATM, Time: 180 ms, Memory: 16872 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		// insertion sort
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i - 1;
+
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+
+		int result = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j <= i; j++) {
+				result += arr[j];
+			}
+		}
+
+		System.out.println(result);
+
+	}
+
+}
