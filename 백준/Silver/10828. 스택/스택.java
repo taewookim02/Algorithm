81d094fa19b70bd7ec6c2acfffb6671293693ece
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-04-08 14:01:21 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> st = new Stack<>();
+		for (int i = 0; i < N; i++) {
+			String[] line = br.readLine().split(" ");
+			if (line.length > 1) {
+				st.push(Integer.parseInt(line[1]));
+				continue;
+			}
+			switch (line[0]) {
+			case "pop":
+//				스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력
+//				정수가 없는 경우에는 -1 출력
+				if (!st.isEmpty()) {					
+					bw.write(String.valueOf(st.pop()));
+					bw.newLine();
+				} else {
+					bw.write(String.valueOf(-1));
+					bw.newLine();
+					
+				}
+				break;
+			case "size":
+//				정수의 개수를 출력
+				bw.write(String.valueOf(st.size()));
+				bw.newLine();
+				break;
+			case "empty":
+//				비어있으면 1, 아니면 0을 출력
+				if (st.isEmpty()) {
+					bw.write(String.valueOf(1));
+				} else {
+					bw.write(String.valueOf(0));
+				}
+				bw.newLine();
+				break;
+			case "top":
+//				가장 위에 있는 정수를 출력
+//				없는 경우에는 -1을 출력
+				if (!st.isEmpty()) {
+					bw.write(String.valueOf(st.peek()));
+				} else {
+					bw.write(String.valueOf(-1));
+				}
+				bw.newLine();
+				break;
+			}
+			
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
