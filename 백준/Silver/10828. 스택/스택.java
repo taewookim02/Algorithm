90945bc2d4ce1a6481069d99f6a6ee5d12d0ec5b
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-04-10 21:39:35 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 18300 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -6,59 +6,58 @@ import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int N = Integer.parseInt(br.readLine());
-		
 		Stack<Integer> st = new Stack<>();
 		for (int i = 0; i < N; i++) {
 			String[] line = br.readLine().split(" ");
+			
 			if (line.length > 1) {
 				st.push(Integer.parseInt(line[1]));
-				continue;
 			}
+			
 			switch (line[0]) {
-			case "pop":
-//				스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력
-//				정수가 없는 경우에는 -1 출력
-				if (!st.isEmpty()) {					
-					bw.write(String.valueOf(st.pop()));
+			case "top":
+//				위에 있는 정수를 출력
+//				없는 경우에는 -1을 출력
+				if (!st.isEmpty()) {
+					bw.write(String.valueOf(st.peek()));
 					bw.newLine();
 				} else {
 					bw.write(String.valueOf(-1));
 					bw.newLine();
-					
 				}
 				break;
 			case "size":
-//				정수의 개수를 출력
+//				정수의 개수를 출력	
 				bw.write(String.valueOf(st.size()));
 				bw.newLine();
 				break;
 			case "empty":
 //				비어있으면 1, 아니면 0을 출력
-				if (st.isEmpty()) {
-					bw.write(String.valueOf(1));
-				} else {
-					bw.write(String.valueOf(0));
-				}
-				bw.newLine();
-				break;
-			case "top":
-//				가장 위에 있는 정수를 출력
-//				없는 경우에는 -1을 출력
 				if (!st.isEmpty()) {
-					bw.write(String.valueOf(st.peek()));
+					bw.write(String.valueOf(0));
+					bw.newLine();
+				} else {
+					bw.write(String.valueOf(1));
+					bw.newLine();
+				}
+				break;
+			case "pop":
+//				정수를 빼고, 그 수를 출력한다
+//				없는 경우에는 -1
+				if (!st.isEmpty()) {
+					bw.write(String.valueOf(st.pop()));
+					bw.newLine();
 				} else {
 					bw.write(String.valueOf(-1));
+					bw.newLine();
 				}
-				bw.newLine();
 				break;
 			}
-			
 		}
 		bw.flush();
 		bw.close();
