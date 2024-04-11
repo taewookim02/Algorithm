06573fa5df31d34bc20d04129ec38c372f611bb7
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-04-11 17:24:16 +0900

    Time: 8 ms (37.58%) | Memory: 46.7 MB (99.92%) - LeetSync

---
+++ Leetcode/169-majority-element/majority-element.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        // sort nums
+        sort(nums, 0, nums.length - 1);
+        int n = nums.length;
+        return nums[n / 2];
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        // get sizes
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // temp arr
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // put in data
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        // init idxes
+        int i = 0, j = 0;
+        int k = l;
+
+        // while 
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // remainders
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static void sort (int[] arr, int l, int r) {
+        if (l < r) {
+            // get middle
+            int m = l + (r - l) / 2;
+
+            // sort 2 halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // merge 2 halves
+            merge(arr, l, m, r);
+        }
+    }
+}
