4105ea5eb6e97d47ea52c353b4f0e316d3b3c562
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-04-17 18:49:18 +0900

    [Silver IV] Title: 괄호, Time: 88 ms, Memory: 11732 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,48 +1,35 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
-		
+
 		for (int i = 0; i < T; i++) {
 			String line = br.readLine();
 			Stack<Character> st = new Stack<>();
-			boolean isOk = true;
-			
+			boolean isProper = true;
 			for (char ch : line.toCharArray()) {
 				if (ch == '(') {
-					st.push(ch);
-				}
-				if (ch == ')') {
-					if (st.size() > 0) {						
-						st.pop();
+					st.push('(');
+				} else if (ch == ')') {
+					if (st.isEmpty()) {
+						isProper = false;
 					} else {
-						isOk = false;
-						break;
+						st.pop();
 					}
 				}
 			}
-			
-			if (!isOk || st.size() > 0) {
-				bw.write("NO");
-				bw.newLine();
+			if (st.size() > 0 || !isProper) {
+				System.out.println("NO");
 			} else {
-				bw.write("YES");
-				bw.newLine();
+				System.out.println("YES");
 			}
 		}
-		
-		
-		bw.flush();
-		bw.close();
 		br.close();
-		
+
 	}
 }
