79f64f38c39817a88cadd90f88f8ccd1bed6ddd8
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-04-17 14:29:38 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.36 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -3,14 +3,13 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-        
+
         // sort A asc
-        sort(A, 0, A.length - 1, true);
+        quickSort(A, 0, A.length - 1, true);
         
-        // sort B asc
-        sort(B, 0, B.length - 1, false);
-    
-        // answer +=
+        // sort B desc
+        quickSort(B, 0, B.length - 1, false);
+        
         for (int i = 0; i < A.length; i++) {
             answer += A[i] * B[i];
         }
@@ -18,92 +17,50 @@ class Solution
         return answer;
     }
     
-    private static void merge(int[] arr, int l, int m, int r, boolean isAsc) {
-        // get sizes
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        
-        // temp arr
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-        
-        // put in data
-        for (int i = 0; i < n1; i++) {
-            L[i] = arr[l + i];
-        }
-        
-        for (int j = 0; j < n2; j++) {
-            R[j] = arr[m + 1 + j];
-        }
-        
-        // init idx
-        int i = 0, j = 0;
-        int k = l;
-        
-        
-        // while (isAsc or desc)
-        if (isAsc) {
-            while (i < n1 && j < n2) {
-                if (L[i] <= R[j]) {
-                    arr[k] = L[i];
-                    i++;
-                } else {
-                    arr[k] = R[j];
-                    j++;
-                }
-                k++;
-            }
-            
-            // remainders
-            while (i < n1) {
-                arr[k] = L[i];
-                i++;
-                k++;
-            }
-            while (j < n2) {
-                arr[k] = R[j];
-                j++;
-                k++;
-            }
-        } else {
-            while (i < n1 && j < n2) {
-                if (L[i] >= R[j]) {
-                    arr[k] = L[i];
-                    i++;
-                } else {
-                    arr[k] = R[j];
-                    j++;
-                }
-                k++;
-            }
-            
-            // remainders
-            while (i < n1) {
-                arr[k] = L[i];
-                i++;
-                k++;
-            }
-            while (j < n2) {
-                arr[k] = R[j];
-                j++;
-                k++;
-            }
-        }
-        
-        
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
-    private static void sort(int[] arr, int l, int r, boolean isAsc) {
-        if (l < r) {
-            // get middle
-            int m = l + (r - l) / 2;
+    static int partition(int[] arr, int low, int high, boolean isAsc) {
+        // get pivot
+        int pivot = arr[high];
+        
+        // idx
+        int i = low - 1;
+        
+        // for and check
+        for (int j = low; j <= high; j++) {
+            if (isAsc) {
+                if (arr[j] < pivot) {
+                    // ++
+                    i++;
+                    swap(arr, i, j);
+                }    
+            } else {
+                if (arr[j] > pivot) {
+                    // ++
+                    i++;
+                    swap(arr, i, j);
+                }   
+            }
             
-            // sort 2 halves
-            sort(arr, l, m, isAsc);
-            sort(arr, m + 1, r, isAsc);
-            
-            merge(arr, l, m, r, isAsc);
         }
+        
+        // remainders
+        swap(arr, i + 1, high);
+        return i + 1;
     }
     
+    static void quickSort(int[] arr, int low, int high, boolean isAsc) {
+        if (low < high) {
+            // get pi
+            int pi = partition(arr, low, high, isAsc);
+            
+            // sort 2
+            quickSort(arr, low, pi - 1, isAsc);
+            quickSort(arr, pi + 1, high, isAsc);
+        }
+    }
 }
