68f214420a2fac3c01d34a468d5ac99a23df6dd0
Tae Woo Kim <65373329+taewookim02@users.noreply.github.com>
2024-04-25 18:49:37 +0900

    [Bronze II] Title: 소수 찾기, Time: 116 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int result = 0;
+		for(int i = 0; i < T; i++) {
+			int n = sc.nextInt();
+			
+			if (prime(n)) result++;
+		}
+		System.out.println(result);
+	}
 
-		int N = sc.nextInt();
-		int[] arr = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
+	static boolean prime(int n) {
+		if (n < 2) {
+			return false;
 		}
 
-		int primeCount = 0;
-		for (int i = 0; i < N; i++) {
-			int count = 0;
-			for (int j = 2; j <= arr[i]; j++) {
-				if (arr[i] % j == 0) {
-					count++;
-				}
+		for (int i = 2; i * i <= n; i++) {
+			if (n % i == 0) {
+				return false;
 			}
-			if (count == 1) {
-				primeCount++;
-			} 
 		}
-		System.out.println(primeCount);
-
+		
+		return true;
 	}
 }
